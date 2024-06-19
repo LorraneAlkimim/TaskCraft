0637959b4e9adab048849986c08ad5b57becb61f
Lorrane Alkimim <lorranealkmim@yahoo.com.br>
2024-06-19 00:34:53 -0300

    refactor: changes auth middleware to get user id

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,13 +1,19 @@
 import jwt from "jsonwebtoken";
 
-function verifyToken(req, res, next) {
+import User from "../models/User.js";
+
+async function verifyToken(req, res, next) {
   const token = req.header("Authorization");
 
   if (!token) return res.status(401).json({ error: "Access denied" });
 
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req.userId = decoded.userId;
+    const userEmail = decoded.email;
+    const userId = await User.findOne({ email: userEmail })
+      .then((user) => user.id)
+      .catch((error) => error);
+    req.userId = userId;
 
     next();
   } catch (error) {
