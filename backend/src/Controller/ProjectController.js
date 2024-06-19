527780d082f587a01f0cd9ba20a735260572b906
Lorrane Alkimim <lorranealkmim@yahoo.com.br>
2024-06-19 10:03:13 -0300

    feat: create routes to get, update and delete one project

--- backend/src/Controller/ProjectController.js
+++ backend/src/Controller/ProjectController.js
@@ -8,7 +8,7 @@ export async function createProject(req, res) {
     const project = await Project.exists({ userId, name });
     if (project) {
       return res.status(400).json({
-        message: "Project with this name already exists.",
+        message: "Project with this name already exists",
       });
     }
 
@@ -31,4 +31,63 @@ export async function listProjects(req, res) {
   } catch (error) {
     res.status(500).json({ error: "Failed to list projects for use" });
   }
+}
+
+export async function getProject(req, res) {
+  try {
+    const id = req.params.projectId;
+    const project = await Project.findById(id);
+    if (!project) {
+      return res.status(404).send("Project not found");
+    }
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get project" });
+  }
+}
+
+export async function updateProject(req, res) {
+  if (!req.body) {
+    res.status(400).send({
+      message: "Data to update can not be empty",
+    });
+  }
+
+  const id = req.params.projectId;
+  await Project.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Project not found`,
+        });
+      } else {
+        res.send({ message: "Project updated successfully" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: `Failed to update project: ${err}`,
+      });
+    });
+}
+
+export async function deleteProject(req, res) {
+  const id = req.params.projectId;
+  await Project.findOneAndDelete(id, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Project not found.`,
+        });
+      } else {
+        res.send({
+          message: "Project deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: `Failed to delete project: ${err}`,
+      });
+    });
 }
--- backend/src/routes/projects.js
+++ backend/src/routes/projects.js
@@ -3,12 +3,18 @@ import express from "express";
 import verifyToken from "../middleware/authMiddleware.js";
 import {
   createProject,
+  getProject,
   listProjects,
+  updateProject,
+  deleteProject,
 } from "../Controller/ProjectController.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createProject);
 router.get("/", verifyToken, listProjects);
+router.get("/:projectId", verifyToken, getProject);
+router.put("/:projectId", verifyToken, updateProject);
+router.delete("/:projectId", verifyToken, deleteProject);
 
 export default router;
