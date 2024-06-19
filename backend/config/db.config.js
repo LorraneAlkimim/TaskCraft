391a0ee11bcb1b88a6157a6f430537bd5bf678f7
Lorrane Alkimim <lorranealkmim@yahoo.com.br>
2024-06-18 22:44:21 -0300

    chore: setup backend

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,11 +1,35 @@
-const {
-  MONGODB_USER,
-  MONGODB_PASSWORD,
-  MONGODB_HOST,
-  MONGODB_PORT,
-  MONGODB_DATABASE,
-} = process.env;
+import mongoose from "mongoose";
 
-module.exports = {
-  url: `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}?authSource=admin`,
+const { MONGODB_URI } = process.env;
+
+const connect = (app) => {
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: false,
+    maxPoolSize: 10,
+  };
+
+  const connectWithRetry = () => {
+    mongoose.Promise = global.Promise;
+
+    console.log("MongoDB connection with retry");
+
+    mongoose
+      .connect(MONGODB_URI, options)
+      .then(() => {
+        console.log("MongoDB is connected");
+        app.emit("ready");
+      })
+      .catch((error) => {
+        console.log(
+          "MongoDB connection unsuccessful, retry after 2 seconds.",
+          error
+        );
+        setTimeout(connectWithRetry, 2000);
+      });
+  };
+  connectWithRetry();
 };
+
+export default { connect };
